ff595d08a9aa14a517be0190203b057b2f955e4f
unknown <robert.drinnan@gmail.com>
2015-10-26 20:09:30 -0700

    Finalized the alogrithms

--- src/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
+++ src/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
@@ -3,7 +3,6 @@ package ca.ubc.ece.cpen221.mp3.graph;
 import ca.ubc.ece.cpen221.mp3.staff.Graph;
 import ca.ubc.ece.cpen221.mp3.staff.Vertex;
 import java.util.*;
-import java.util.Map.Entry;
 
 /**
  * 
@@ -13,171 +12,163 @@ import java.util.Map.Entry;
 
 public class Algorithms {
 
-	/**
-	 * 
-	 * @param graph
-	 *            An unweighted graph
-	 * @param a
-	 *            A vertex in the graph graph
-	 * @param b
-	 *            A vertex in the graph graph
-	 * @return The number of edges in the shortest path between a and b
-	 */
-	public static int shortestDistance(Graph graph, Vertex a, Vertex b) {
-		Map<Vertex, Boolean> visited = new HashMap<>();
-		Map<Vertex, Vertex> parent = new HashMap<>();
-		Queue<Vertex> vertexQueue = new LinkedList<>();
-		Vertex currentVertex = a;
-		vertexQueue.add(currentVertex);
-		visited.put(currentVertex, true);
-		while (!vertexQueue.isEmpty()) {
-			currentVertex = vertexQueue.remove();
-			if (currentVertex.equals(b)) {
-				break;
-			} else {
-				for (Vertex v : graph.getDownstreamNeighbors(currentVertex)) {
-					if (!visited.containsKey(v)) {
-						vertexQueue.add(v);
-						visited.put(v, true);
-						parent.put(v, currentVertex);
-					}
-				}
-			}
-		}
-		int shortestPathLenght = 0;
-		for (Vertex v = b; v != null; v = parent.get(v)) {
-			shortestPathLenght++;
-		}
-		return shortestPathLenght - 1;
-	}
+    /**
+     * 
+     * @param graph
+     *            An unweighted graph
+     * @param a
+     *            A vertex in the graph graph
+     * @param b
+     *            A vertex in the graph graph
+     * @return The number of edges in the shortest path between a and b
+     */
+    public static int shortestDistance(Graph graph, Vertex a, Vertex b) {
+        Map<Vertex, Boolean> visited = new HashMap<>();
+        Map<Vertex, Vertex> parent = new HashMap<>();
+        Queue<Vertex> vertexQueue = new LinkedList<>();
+        Vertex currentVertex = a;
+        vertexQueue.add(currentVertex);
+        visited.put(currentVertex, true);
+        while (!vertexQueue.isEmpty()) {
+            currentVertex = vertexQueue.remove();
+            if (currentVertex.equals(b)) {
+                break;
+            } else {
+                for (Vertex v : graph.getDownstreamNeighbors(currentVertex)) {
+                    if (!visited.containsKey(v)) {
+                        vertexQueue.add(v);
+                        visited.put(v, true);
+                        parent.put(v, currentVertex);
+                    }
+                }
+            }
+        }
+        int shortestPathLenght = 0;
+        for (Vertex v = b; v != null; v = parent.get(v)) {
+            shortestPathLenght++;
+        }
+        return shortestPathLenght - 1;
+    }
 
-	/**
-	 * 
-	 * @param g
-	 *            An unweighted graph
-	 * @return A set all the possible BFS searches staring at every vertex in
-	 *         graph
-	 * 
-	 */
-	public static Set<List<Vertex>> BFS(Graph g) {
-		Set<List<Vertex>> bfsResults = new HashSet<>();
+    /**
+     * 
+     * @param g
+     *            An unweighted graph
+     * @return A set all the possible BFS searches staring at every vertex in
+     *         graph
+     * 
+     */
+    public static Set<List<Vertex>> BFS(Graph g) {
+        Set<List<Vertex>> bfsResults = new HashSet<>();
+        for (Vertex v : g.getVertices()) {
+            Queue<Vertex> vertexQueue = new LinkedList<>();
+            List<Vertex> visited = new LinkedList<>();
+            visited.add(v);
+            vertexQueue.add(v);
+            while (!(vertexQueue.isEmpty())) {
+                Vertex currentVertex = vertexQueue.poll();
+                for (Vertex w : g.getDownstreamNeighbors(currentVertex)) {
+                    if (!(visited.contains(w))) {
+                        visited.add(w);
+                        vertexQueue.add(w);
+                    }
+                }
+            }
+            bfsResults.add(visited);
+        }
+        return bfsResults;
 
-		for (Vertex v : g.getVertices()) {
-			List<Vertex> resultsForV = new ArrayList<>();
-			Map<Vertex, Boolean> visited = new HashMap<>();
-			Map<Vertex, Vertex> parent = new HashMap<>();
-			Queue<Vertex> vertexQueue = new LinkedList<>();
-			Vertex currentVertex = v;
-			visited.put(currentVertex, true);
-			vertexQueue.add(currentVertex);
-			while (!vertexQueue.isEmpty()) {
-				currentVertex = vertexQueue.remove();
-				for (Vertex n : g.getDownstreamNeighbors(currentVertex)) {
-					if (!visited.containsKey(n)) {
-						vertexQueue.add(n);
-						visited.put(n, true);
-						parent.put(n, currentVertex);
-					}
+    }
 
-				}
-			}
-			for (Entry<Vertex, Vertex> results:parent.entrySet()) {
-				resultsForV.add(results.getValue());
-			}
-			bfsResults.add(resultsForV);
-		}
-		return bfsResults;
-	}
+    /**
+     * 
+     * @param g
+     *            An unweighted graph
+     * @return A set of all possible DFS searches starting at every vertex in
+     *         graph
+     */
+    public static Set<List<Vertex>> DFS(Graph g) {
+        Set<List<Vertex>> dfsResults = new HashSet<>();
+        for (Vertex v : g.getVertices()) {
+            Stack<Vertex> vertexStack = new Stack<>();
+            List<Vertex> visited = new LinkedList<>();
+            vertexStack.push(v);
+            while (!(vertexStack.isEmpty())) {
+                Vertex currentVertex = vertexStack.pop();
+                if (!(visited.contains(currentVertex))) {
+                    visited.add(currentVertex);
+                }
+                for (Vertex w : g.getDownstreamNeighbors(currentVertex)) {
+                    vertexStack.add(w);
+                }
+            }
+            dfsResults.add(visited);
+        }
+        return dfsResults;
 
-	/**
-	 * 
-	 * @param g
-	 *            An unweighted graph
-	 * @return A set of all possible DFS searches starting at every vertex in
-	 *         graph
-	 */
-	public static Set<List<Vertex>> DFS(Graph g) {
-		Set<List<Vertex>> DFSResults = new HashSet<>();
-		for (Vertex v : g.getVertices()) {
-			DFSResults.add(dfs(g, v));
-		}
-		return DFSResults;
-	}
+    }
 
-	/**
-	 * 
-	 * @param g
-	 *            an unweighted graph
-	 * @param a
-	 *            A vertex in the g
-	 * @param b
-	 *            A vertex in the g
-	 * @return returns a List of all vertices w that have an edges that goes
-	 *         from w to v, returns an empty list if no vertex w exist.
-	 */
-	public static List<Vertex> commonUpstreamVertices(Graph g, Vertex a, Vertex b) {
-		List<Vertex> upstreamResults = new ArrayList<>();
-		List<Vertex> aVertices = g.getUpstreamNeighbors(a);
-		List<Vertex> bVertices = g.getUpstreamNeighbors(b);
-		if (aVertices.size() > bVertices.size()) {
-			for (Vertex v : aVertices) {
-				if (bVertices.contains(v)) {
-					upstreamResults.add(v);
-				}
-			}
-		} else {
-			for (Vertex w : bVertices) {
-				if (aVertices.contains(w)) {
-					upstreamResults.add(w);
-				}
-			}
-		}
-		return upstreamResults;
-	}
+    /**
+     * 
+     * @param g
+     *            an unweighted graph
+     * @param a
+     *            A vertex in the g
+     * @param b
+     *            A vertex in the g
+     * @return returns a List of all vertices w that have an edges that goes
+     *         from w to v, returns an empty list if no vertex w exist.
+     */
+    public static List<Vertex> commonUpstreamVertices(Graph g, Vertex a, Vertex b) {
+        List<Vertex> upstreamResults = new ArrayList<>();
+        List<Vertex> aVertices = g.getUpstreamNeighbors(a);
+        List<Vertex> bVertices = g.getUpstreamNeighbors(b);
+        if (aVertices.size() > bVertices.size()) {
+            for (Vertex v : aVertices) {
+                if (bVertices.contains(v)) {
+                    upstreamResults.add(v);
+                }
+            }
+        } else {
+            for (Vertex w : bVertices) {
+                if (aVertices.contains(w)) {
+                    upstreamResults.add(w);
+                }
+            }
+        }
+        return upstreamResults;
+    }
 
-	/**
-	 * 
-	 * @param g
-	 *            An unweighted graph
-	 * @param a
-	 *            A vertex in g
-	 * @param b
-	 *            A vertex in g
-	 * @return returns a List of all vertices w that have an edges that goes
-	 *         from v to w, returns an empty list if no vertex w exist.
-	 */
-	public static List<Vertex> commonDownstreamVertices(Graph g, Vertex a, Vertex b) {
-		List<Vertex> downstreamResults = new ArrayList<>();
-		List<Vertex> aVertices = g.getDownstreamNeighbors(a);
-		List<Vertex> bVertices = g.getDownstreamNeighbors(b);
-		if (aVertices.size() > bVertices.size()) {
-			for (Vertex v : aVertices) {
-				if (bVertices.contains(v)) {
-					downstreamResults.add(v);
-				}
-			}
-		} else {
-			for (Vertex w : bVertices) {
-				if (aVertices.contains(w)) {
-					downstreamResults.add(w);
-				}
-			}
-		}
+    /**
+     * 
+     * @param g
+     *            An unweighted graph
+     * @param a
+     *            A vertex in g
+     * @param b
+     *            A vertex in g
+     * @return returns a List of all vertices w that have an edges that goes
+     *         from v to w, returns an empty list if no vertex w exist.
+     */
+    public static List<Vertex> commonDownstreamVertices(Graph g, Vertex a, Vertex b) {
+        List<Vertex> downstreamResults = new ArrayList<>();
+        List<Vertex> aVertices = g.getDownstreamNeighbors(a);
+        List<Vertex> bVertices = g.getDownstreamNeighbors(b);
+        if (aVertices.size() > bVertices.size()) {
+            for (Vertex v : aVertices) {
+                if (bVertices.contains(v)) {
+                    downstreamResults.add(v);
+                }
+            }
+        } else {
+            for (Vertex w : bVertices) {
+                if (aVertices.contains(w)) {
+                    downstreamResults.add(w);
+                }
+            }
+        }
 
-		return downstreamResults;
-	}
+        return downstreamResults;
+    }
 
-	/**
-	 * A helper method for DFS
-	 */
-	private static List<Vertex> dfs(Graph g, Vertex v) {
-		List<Vertex> dfsResults = new ArrayList<>();
-		dfsResults.add(v);
-		for (Vertex w : g.getDownstreamNeighbors(v)) {
-			if (dfsResults.contains(w)) {
-				dfs(g, w);
-			}
-		}
-		return dfsResults;
-	}
 }
--- src/ca/ubc/ece/cpen221/mps/tests/algorithmTest.java
+++ src/ca/ubc/ece/cpen221/mps/tests/algorithmTest.java
@@ -19,182 +19,177 @@ import ca.ubc.ece.cpen221.mp3.staff.Vertex;
 
 public class algorithmTest {
 
-	private Graph testGraph = new AdjacencyListGraph();
+    private Graph testGraph = new AdjacencyListGraph();
 
-	@Test
-	public void shortesDistanceTest() {
+    @Test
+    public void shortesDistanceTest() {
 
-		Vertex test1 = new Vertex("test1");
-		Vertex test2 = new Vertex("test2");
-		Vertex test3 = new Vertex("test3");
-		Vertex test4 = new Vertex("test4");
-		Vertex test5 = new Vertex("test5");
-		Vertex test6 = new Vertex("test6");
-		Vertex test7 = new Vertex("test7");
+        Vertex test1 = new Vertex("test1");
+        Vertex test2 = new Vertex("test2");
+        Vertex test3 = new Vertex("test3");
+        Vertex test4 = new Vertex("test4");
+        Vertex test5 = new Vertex("test5");
+        Vertex test6 = new Vertex("test6");
+        Vertex test7 = new Vertex("test7");
 
-		testGraph.addVertex(test1);
-		testGraph.addVertex(test2);
-		testGraph.addVertex(test3);
-		testGraph.addVertex(test4);
-		testGraph.addVertex(test5);
-		testGraph.addVertex(test6);
-		testGraph.addVertex(test7);
+        testGraph.addVertex(test1);
+        testGraph.addVertex(test2);
+        testGraph.addVertex(test3);
+        testGraph.addVertex(test4);
+        testGraph.addVertex(test5);
+        testGraph.addVertex(test6);
+        testGraph.addVertex(test7);
 
-		testGraph.addEdge(test1, test2);
-		testGraph.addEdge(test1, test3);
-		testGraph.addEdge(test1, test4);
-		testGraph.addEdge(test2, test3);
-		testGraph.addEdge(test4, test5);
-		testGraph.addEdge(test5, test2);
-		testGraph.addEdge(test6, test7);
+        testGraph.addEdge(test1, test2);
+        testGraph.addEdge(test1, test3);
+        testGraph.addEdge(test1, test4);
+        testGraph.addEdge(test2, test3);
+        testGraph.addEdge(test4, test5);
+        testGraph.addEdge(test5, test2);
+        testGraph.addEdge(test6, test7);
 
-		assertEquals(1, Algorithms.shortestDistance(testGraph, test1, test3));
-		assertEquals(3, Algorithms.shortestDistance(testGraph, test4, test3));
-		assertEquals(1, Algorithms.shortestDistance(testGraph, test6, test7));
-		assertEquals(0, Algorithms.shortestDistance(testGraph, test5, test6));
-		assertEquals(1, Algorithms.shortestDistance(testGraph, test1, test2));
-		assertEquals(0, Algorithms.shortestDistance(testGraph, test1, test1));
-	}
+        assertEquals(1, Algorithms.shortestDistance(testGraph, test1, test3));
+        assertEquals(3, Algorithms.shortestDistance(testGraph, test4, test3));
+        assertEquals(1, Algorithms.shortestDistance(testGraph, test6, test7));
+        assertEquals(0, Algorithms.shortestDistance(testGraph, test5, test6));
+        assertEquals(1, Algorithms.shortestDistance(testGraph, test1, test2));
+        assertEquals(0, Algorithms.shortestDistance(testGraph, test1, test1));
+    }
 
-	@Test
-	public void commonDownstreamVerticesTest() {
+    @Test
+    public void commonDownstreamVerticesTest() {
 
-		Vertex test1 = new Vertex("test1");
-		Vertex test2 = new Vertex("test2");
-		Vertex test3 = new Vertex("test3");
-		Vertex test4 = new Vertex("test4");
-		Vertex test5 = new Vertex("test5");
-		Vertex test6 = new Vertex("test6");
-		Vertex test7 = new Vertex("test7");
+        Vertex test1 = new Vertex("test1");
+        Vertex test2 = new Vertex("test2");
+        Vertex test3 = new Vertex("test3");
+        Vertex test4 = new Vertex("test4");
+        Vertex test5 = new Vertex("test5");
+        Vertex test6 = new Vertex("test6");
+        Vertex test7 = new Vertex("test7");
 
-		testGraph.addVertex(test1);
-		testGraph.addVertex(test2);
-		testGraph.addVertex(test3);
-		testGraph.addVertex(test4);
-		testGraph.addVertex(test5);
-		testGraph.addVertex(test6);
-		testGraph.addVertex(test7);
+        testGraph.addVertex(test1);
+        testGraph.addVertex(test2);
+        testGraph.addVertex(test3);
+        testGraph.addVertex(test4);
+        testGraph.addVertex(test5);
+        testGraph.addVertex(test6);
+        testGraph.addVertex(test7);
 
-		testGraph.addEdge(test1, test2);
-		testGraph.addEdge(test1, test3);
-		testGraph.addEdge(test1, test4);
-		testGraph.addEdge(test2, test3);
-		testGraph.addEdge(test4, test5);
-		testGraph.addEdge(test5, test2);
-		testGraph.addEdge(test5, test3);
-		testGraph.addEdge(test6, test7);
+        testGraph.addEdge(test1, test2);
+        testGraph.addEdge(test1, test3);
+        testGraph.addEdge(test1, test4);
+        testGraph.addEdge(test2, test3);
+        testGraph.addEdge(test4, test5);
+        testGraph.addEdge(test5, test2);
+        testGraph.addEdge(test5, test3);
+        testGraph.addEdge(test6, test7);
 
-		List<Vertex> expectedResults1 = new LinkedList<Vertex>();
-		List<Vertex> expectedResults2 = new LinkedList<Vertex>();
-		List<Vertex> expectedResults3 = new LinkedList<Vertex>();
-		expectedResults1.add(test3);
-		expectedResults3.add(test2);
-		expectedResults3.add(test3);
+        List<Vertex> expectedResults1 = new LinkedList<Vertex>();
+        List<Vertex> expectedResults2 = new LinkedList<Vertex>();
+        List<Vertex> expectedResults3 = new LinkedList<Vertex>();
+        expectedResults1.add(test3);
+        expectedResults3.add(test2);
+        expectedResults3.add(test3);
 
-		assertEquals(true, Algorithms.commonDownstreamVertices(testGraph, test1, test2).containsAll(expectedResults1));
-		assertEquals(true, Algorithms.commonDownstreamVertices(testGraph, test1, test4).containsAll(expectedResults2));
-		assertEquals(true, Algorithms.commonDownstreamVertices(testGraph, test1, test5).containsAll(expectedResults3));
+        assertEquals(true, Algorithms.commonDownstreamVertices(testGraph, test1, test2).containsAll(expectedResults1));
+        assertEquals(true, Algorithms.commonDownstreamVertices(testGraph, test1, test4).containsAll(expectedResults2));
+        assertEquals(true, Algorithms.commonDownstreamVertices(testGraph, test1, test5).containsAll(expectedResults3));
 
-	}
+    }
 
-	@Test
-	public void commonUpstreamVerticesTest() {
-		Vertex test1 = new Vertex("test1");
-		Vertex test2 = new Vertex("test2");
-		Vertex test3 = new Vertex("test3");
-		Vertex test4 = new Vertex("test4");
-		Vertex test5 = new Vertex("test5");
-		Vertex test6 = new Vertex("test6");
-		Vertex test7 = new Vertex("test7");
+    @Test
+    public void commonUpstreamVerticesTest() {
+        Vertex test1 = new Vertex("test1");
+        Vertex test2 = new Vertex("test2");
+        Vertex test3 = new Vertex("test3");
+        Vertex test4 = new Vertex("test4");
+        Vertex test5 = new Vertex("test5");
+        Vertex test6 = new Vertex("test6");
+        Vertex test7 = new Vertex("test7");
 
-		testGraph.addVertex(test1);
-		testGraph.addVertex(test2);
-		testGraph.addVertex(test3);
-		testGraph.addVertex(test4);
-		testGraph.addVertex(test5);
-		testGraph.addVertex(test6);
-		testGraph.addVertex(test7);
+        testGraph.addVertex(test1);
+        testGraph.addVertex(test2);
+        testGraph.addVertex(test3);
+        testGraph.addVertex(test4);
+        testGraph.addVertex(test5);
+        testGraph.addVertex(test6);
+        testGraph.addVertex(test7);
 
-		testGraph.addEdge(test1, test2);
-		testGraph.addEdge(test1, test3);
-		testGraph.addEdge(test1, test4);
-		testGraph.addEdge(test2, test3);
-		testGraph.addEdge(test4, test5);
-		testGraph.addEdge(test5, test2);
-		testGraph.addEdge(test5, test3);
-		testGraph.addEdge(test6, test7);
+        testGraph.addEdge(test1, test2);
+        testGraph.addEdge(test1, test3);
+        testGraph.addEdge(test1, test4);
+        testGraph.addEdge(test2, test3);
+        testGraph.addEdge(test4, test5);
+        testGraph.addEdge(test5, test2);
+        testGraph.addEdge(test5, test3);
+        testGraph.addEdge(test6, test7);
 
-		List<Vertex> expectedResults1 = new LinkedList<Vertex>();
-		List<Vertex> expectedResults2 = new LinkedList<Vertex>();
-		List<Vertex> expectedResults3 = new LinkedList<Vertex>();
+        List<Vertex> expectedResults1 = new LinkedList<Vertex>();
+        List<Vertex> expectedResults2 = new LinkedList<Vertex>();
+        List<Vertex> expectedResults3 = new LinkedList<Vertex>();
 
-		expectedResults1.add(test1);
-		expectedResults3.add(test1);
-		expectedResults3.add(test5);
+        expectedResults1.add(test1);
+        expectedResults3.add(test1);
+        expectedResults3.add(test5);
 
-		assertEquals(true, Algorithms.commonUpstreamVertices(testGraph, test4, test3).containsAll(expectedResults1));
-		assertEquals(true, Algorithms.commonUpstreamVertices(testGraph, test7, test4).containsAll(expectedResults2));
-		assertEquals(true, Algorithms.commonUpstreamVertices(testGraph, test2, test3).containsAll(expectedResults3));
+        assertEquals(true, Algorithms.commonUpstreamVertices(testGraph, test4, test3).containsAll(expectedResults1));
+        assertEquals(true, Algorithms.commonUpstreamVertices(testGraph, test7, test4).containsAll(expectedResults2));
+        assertEquals(true, Algorithms.commonUpstreamVertices(testGraph, test2, test3).containsAll(expectedResults3));
 
-	}
+    }
 
-	@Test
-	public void BfsTest() {
-		Vertex test1 = new Vertex("test1");
-		Vertex test2 = new Vertex("test2");
-		Vertex test3 = new Vertex("test3");
-		Vertex test4 = new Vertex("test4");
-		Vertex test5 = new Vertex("test5");
-		Vertex test6 = new Vertex("test6");
-		Vertex test7 = new Vertex("test7");
+    @Test
+    public void bfsTest() {
+        Vertex a = new Vertex("a");
+        Vertex b = new Vertex("b");
+        Vertex c = new Vertex("c");
+        Vertex d = new Vertex("d");
+        Vertex e = new Vertex("e");
+        Vertex f = new Vertex("f");
+  
+        testGraph.addVertex(a);
+        testGraph.addVertex(b);
+        testGraph.addVertex(c);
+        testGraph.addVertex(d);
+        testGraph.addVertex(e);
+        testGraph.addVertex(f);
 
-		testGraph.addVertex(test1);
-		testGraph.addVertex(test2);
-		testGraph.addVertex(test3);
-		testGraph.addVertex(test4);
-		testGraph.addVertex(test5);
-		testGraph.addVertex(test6);
-		testGraph.addVertex(test7);
+        testGraph.addEdge(a, b);
+        testGraph.addEdge(a, c);
+        testGraph.addEdge(b, d);
+        testGraph.addEdge(b, e);
+        testGraph.addEdge(d, f);
 
-		testGraph.addEdge(test1, test2);
-		testGraph.addEdge(test1, test3);
-		testGraph.addEdge(test1, test4);
-		testGraph.addEdge(test2, test3);
-		testGraph.addEdge(test4, test5);
-		testGraph.addEdge(test5, test2);
-		testGraph.addEdge(test5, test3);
-		testGraph.addEdge(test6, test7);
+        
 
-		assertEquals(true, Algorithms.BFS(testGraph));
-	}
 
-//	@Test
-//	public void DfsTest() {
-//		Vertex test1 = new Vertex("test1");
-//		Vertex test2 = new Vertex("test2");
-//		Vertex test3 = new Vertex("test3");
-//		Vertex test4 = new Vertex("test4");
-//		Vertex test5 = new Vertex("test5");
-//		Vertex test6 = new Vertex("test6");
-//		Vertex test7 = new Vertex("test7");
-//
-//		testGraph.addVertex(test1);
-//		testGraph.addVertex(test2);
-//		testGraph.addVertex(test3);
-//		testGraph.addVertex(test4);
-//		testGraph.addVertex(test5);
-//		testGraph.addVertex(test6);
-//		testGraph.addVertex(test7);
-//
-//		testGraph.addEdge(test1, test2);
-//		testGraph.addEdge(test1, test3);
-//		testGraph.addEdge(test1, test4);
-//		testGraph.addEdge(test2, test3);
-//		testGraph.addEdge(test4, test5);
-//		testGraph.addEdge(test5, test2);
-//		testGraph.addEdge(test5, test3);
-//		testGraph.addEdge(test6, test7);
-//
-//		assertEquals(true, Algorithms.DFS(testGraph));
-//	}
+        assertEquals(6, Algorithms.BFS(testGraph).toString());
+    }
+    @Test
+    public void dfsTest() {
+        Vertex a = new Vertex("a");
+        Vertex b = new Vertex("b");
+        Vertex c = new Vertex("c");
+        Vertex d = new Vertex("d");
+        Vertex e = new Vertex("e");
+        Vertex f = new Vertex("f");
+  
+        testGraph.addVertex(a);
+        testGraph.addVertex(b);
+        testGraph.addVertex(c);
+        testGraph.addVertex(d);
+        testGraph.addVertex(e);
+        testGraph.addVertex(f);
+
+        testGraph.addEdge(a, b);
+        testGraph.addEdge(a, c);
+        testGraph.addEdge(b, d);
+        testGraph.addEdge(b, e);
+        testGraph.addEdge(d, f);
+
+        
+
+
+        assertEquals(6, Algorithms.DFS(testGraph).toString());
+    }
 }
